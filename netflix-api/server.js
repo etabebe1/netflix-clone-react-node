f8280468a32b1f8edb4fde2744982a4350351ecc
etabebe1 <etabebe1@gmail.com>
2024-04-26 06:07:52 -0400

    full stack  update

---
+++ netflix-api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+// FIXME: MONGO_URI
+mongoose
+  .connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.4", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
---
+++ netflix-ui/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "36248180a32bcc986343b9f109c8e9ca";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
