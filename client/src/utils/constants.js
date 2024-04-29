cef97441ef25f03a85321ed819806d51d2d384e1
etabebe1 <etabebe1@gmail.com>
2024-04-29 14:37:41 -0400

    base backend url updated

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const API_KEY = "36248180a32bcc986343b9f109c8e9ca";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const baseUrl = "www.netflix-clone-api";
+export const baseUrl = "https://netflix-clone-api-henna.vercel.app";
